27d2e9248ec296baff7eeb165701a8f8ae765dd2
Anant Kumar <anantkumar012002@gmail.com>
2024-04-27 18:47:07 +0530

    home page ui

---
+++ src/components/Comman/NavBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+  return (
+    <>
+      <div className="fixed left-0 top-0 h-6 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit   ">
+        Get started by editing&nbsp;
+        <code className="font-mono font-bold">src/app/page.tsx</code>
+      </div>
+    </>
+  );
+}
